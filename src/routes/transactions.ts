8204af115aaef55d011c527b54f25458667ba316
Leandro Batista <leandrobouwierphp@gmail.com>
2025-09-23 17:53:21 -0300

    get id, list transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,28 +1,47 @@
-import { FastifyInstance } from "fastify"
-import { z } from 'zod'
-import { knex } from "../database"
-import { randomUUID } from 'node:crypto'
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { knex } from "../database";
+import { randomUUID } from "node:crypto";
 
 // Request Body: HTTPs -> Serve para criar ou editar algum recurso.
 
-export async function transactionsRoutes(app: FastifyInstance){
-    app.post('/', async (request, reply) => {
+export async function transactionsRoutes(app: FastifyInstance) {
 
-      const createTransactionBodySchema = z.object({
-        title: z.string(),
-        amount: z.number(),
-        type: z.enum(['credit', 'debit'])
-      })
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
 
-      const {title, amount, type } = createTransactionBodySchema.parse(request.body,)
-        
-      await knex('transactions').insert({
-        id: randomUUID(),
-        title,
-        amount: type === 'credit' ? amount : amount * -1, 
-      })
-      
-      return reply.status(201).send()
+    return { transactions }
+  })
 
-      })
+  app.get('/:id', async (request) =>{
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const {id} = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return { transaction }
+  })
+
+  app.post("/", async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(["credit", "debit"]),
+    });
+
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body
+    );
+
+    await knex("transactions").insert({
+      id: randomUUID(),
+      title,
+      amount: type === "credit" ? amount : amount * -1,
+    });
+
+    return reply.status(201).send();
+  });
 }
