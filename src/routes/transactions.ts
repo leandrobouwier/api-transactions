49283d369b23059e1185dff7b2dd2ba1a906fb2d
Leandro Batista <leandrobouwierphp@gmail.com>
2025-09-23 22:56:03 -0300

    resumo da conta do usuario, soma

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -7,31 +7,38 @@ import { randomUUID } from "node:crypto";
 
 export async function transactionsRoutes(app: FastifyInstance) {
 
+  // O usuário deve poder criar uma nova transação;
   app.get('/', async () => {
     const transactions = await knex('transactions').select()
 
     return { transactions }
   })
 
-  app.get('/:id', async (request) =>{
+  // Pegar transação por id - O usuário deve poder visualizar uma transação única;
+  app.get('/:id', async (request) => {
     const getTransactionParamsSchema = z.object({
       id: z.string().uuid(),
     })
 
-    const {id} = getTransactionParamsSchema.parse(request.params)
-
+    const { id } = getTransactionParamsSchema.parse(request.params)
     const transaction = await knex('transactions').where('id', id).first()
-
     return { transaction }
   })
 
+  app.get('/summary', async () =>{
+    const summary = await knex('transactions').sum('amount', { as: 'amount' }).first()
+
+    return summary;
+  })
+
+  // validação de transação com zod - O usuário deve poder criar uma nova transação;
   app.post("/", async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
       amount: z.number(),
       type: z.enum(["credit", "debit"]),
     });
-
+    
     const { title, amount, type } = createTransactionBodySchema.parse(
       request.body
     );
@@ -40,6 +47,7 @@ export async function transactionsRoutes(app: FastifyInstance) {
       id: randomUUID(),
       title,
       amount: type === "credit" ? amount : amount * -1,
+      //A transação pode ser do tipo crédito que somará ao valor total, ou débito subtrairá;
     });
 
     return reply.status(201).send();
